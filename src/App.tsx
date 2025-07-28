2dc30569274e67463ad6b85b2e7f89829d78cdad
Zaven477 <Papikyan86@bk.ru>
2025-07-28 23:29:25 +0300

    add world

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export const AppData = () => {
   return (
     <div>
       <h1>{data}</h1>
-      <button onClick={refetch}>Hello Kar</button>
+      <button onClick={refetch}>World</button>
     </div>
   );
 };
