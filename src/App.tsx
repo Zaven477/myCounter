5e3c9fd4b990e9a2aa53362fee26d50b40e9dd06
Zaven477 <Papikyan86@bk.ru>
2025-08-06 23:42:11 +0300

    add hello

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export const AppData = () => {
   return (
     <div>
       <h1>{data}</h1>
-      <button onClick={refetch}>Click</button>
+      <button onClick={refetch}>Hello world</button>
     </div>
   );
 };
