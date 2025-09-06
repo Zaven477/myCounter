ea76b1f14f7f7d245582f0182f4b0c0de2cde48b
Zaven477 <Papikyan86@bk.ru>
2025-09-06 23:01:37 +0300

    add new chnages

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ export const AppData = () => {
     <div>
       <h1>{data}</h1>
       <button onClick={refetch}>Обновить</button>
-      <button>ella</button>
+      <button>mama</button>
     </div>
   );
 };
