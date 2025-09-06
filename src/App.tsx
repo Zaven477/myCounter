18fd55796058e544c407cab2c5f3a049104064eb
Zaven477 <Papikyan86@bk.ru>
2025-09-06 22:55:10 +0300

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ export const AppData = () => {
     <div>
       <h1>{data}</h1>
       <button onClick={refetch}>Обновить</button>
-      <button>zav</button>
+      <button>ella</button>
     </div>
   );
 };
