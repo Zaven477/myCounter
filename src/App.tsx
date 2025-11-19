e85ed18dd0b18fdf971ae4f44972a47a8af513f3
Zaven477 <Papikyan86@bk.ru>
2025-11-19 23:33:44 +0300

    add new data

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export const AppData = () => {
   return (
     <div>
       <h1>{data}</h1>
-      <button onClick={refetch}>Обновит</button>
+      <button onClick={refetch}>Обновить</button>
       <span>ella</span>
     </div>
   );
