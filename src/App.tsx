9bc0cedfdcc32c4d20ce284a4b6a552b3667f450
Zaven477 <Papikyan86@bk.ru>
2025-11-30 22:24:50 +0300

    add kar

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const AppData = () => {
       <h1>{data}</h1>
       <button onClick={refetch}>Обновить</button>
       <button>Click</button>
-      <div>zav</div>
+      <div>kar</div>
     </div>
   );
 };
