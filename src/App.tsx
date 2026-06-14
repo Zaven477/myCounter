7012423898d66698668ceb438ba25fd7d8760a0f
Zaven477 <Papikyan86@bk.ru>
2026-06-14 14:02:27 +0300

    add new good

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const AppData = () => {
       <h1>{data}</h1>
       <button onClick={refetch}>Обновить</button>
       <div>kar</div>
-      <div>Tig</div>
+      <div>Good</div>
     </div>
   );
 };
