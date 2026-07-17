0902cfe07c9a0850b7527c7b0d5a4eeefdc290f1
Zaven477 <Papikyan86@bk.ru>
2026-07-17 21:01:43 +0300

    remove yes

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,6 @@ export const AppData = () => {
       <button onClick={refetch}>Обновить</button>
       <div>el</div>
       <button>Click</button>
-      <button>yes</button>
     </div>
   );
 };
