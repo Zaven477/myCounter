017b409e0c4fa265a368dc48b528f3b4147ce5d5
Zaven477 <Papikyan86@bk.ru>
2026-09-11 11:48:49 +0300

    remove all

--- src/App.tsx
+++ src/App.tsx
@@ -63,9 +63,6 @@ export const AppData = () => {
     <div>
       <h1>{data}</h1>
       <button onClick={refetch}>Обновить</button>
-      <div>zav</div>
-      <div>el</div>
-      <div>kar</div>
     </div>
   );
 };
